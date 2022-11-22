6015ebb64b37a5c1f32c92b2e496589f51d0f9ad
Parvaz.Behzad <Parvaz.Behzad@okco.ir>
2022-11-22 08:42:53 +0330

    init project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp1;
+using System;
+
+Sample01 sample01 = new Sample01();
+//await sample01.Print("Hello");
+//await sample01.Print("world");
+
+var result = sample01.Print("Hello");
+var result01 = sample01.Print("world");
+await result;
+await result01;
---
+++ ConsoleApp1/ConsoleApp1/Sample01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Sample01
+    {
+        public async Task Start()
+        {
+            Console.WriteLine("Start");
+            await FinalResultCaller();
+        }
+        public async Task FinalResultCaller()
+        {
+            var result = await finalResult(2);
+            Console.WriteLine($"Result is {result}");
+        }
+        public Task<int> finalResult(int sleepFor)
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(1000 * sleepFor);
+                return 10;
+            });
+        }
+
+        public async Task Print(string message)
+        {
+            await Task.Delay(3000);
+            Console.WriteLine(message);
+        }
+    }
+
+
+
+}
